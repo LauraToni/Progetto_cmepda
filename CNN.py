5d406d2ba10f4be7a2ba8f9bfa010216cdc68a08
Maria Irene Tenerani <m.tenerani2@studenti.unipi.it>
2022-01-26 12:50:52 +0100

    Debug

--- CNN.py
+++ CNN.py
@@ -16,7 +16,7 @@ except:
 
 from data_augmentation import VolumeAugmentation
 from input_dati import read_dataset,import_csv, cut_file_name
-from statistics import roc_curve
+from statistics import roc_curve, plot_cv_roc
 
 #Attivare il comando sottostante per utilizzare plaidml
 #os.environ["KERAS_BACKEND"] = "plaidml.keras.backend"
@@ -64,7 +64,7 @@ def stack_train_augmentation(img, img_aug, lbs, lbs_aug):
     lbs_tot=np.append(lbs, lbs_aug, axis=0)
     return img_tot, lbs_tot
 
-def get_model(width=128, height=128, depth=64):
+def inner_model(width=128, height=128, depth=64):
     """
     Built a 3D CNN model.
     Parameters
@@ -83,33 +83,33 @@ def get_model(width=128, height=128, depth=64):
 
     """
 
-    inputs = tensorflow.keras.Input((width, height, depth, 1))
+    input1 = tf.keras.Input((width, height, depth, 1))
 
-    x = tensorflow.keras.layers.Conv3D(filters=32, kernel_size=3, activation="relu",kernel_regularizer=tensorflow.keras.regularizers.l2(l2=1e-3))(inputs)
-    x= tensorflow.keras.layers.ReLU()(x)
-    x = tensorflow.keras.layers.Conv3D(filters=32, kernel_size=3, activation="relu", kernel_regularizer=tensorflow.keras.regularizers.l2(l2=1e-3))(x)
-    x = tensorflow.keras.layers.BatchNormalization(axis=2)(x)
-    x= tensorflow.keras.layers.ReLU()(x)
-    x = tensorflow.keras.layers.MaxPool3D(pool_size=2,  strides=2)(x)
+    conv1 = tf.keras.layers.Conv3D(filters=32, kernel_size=3, activation="relu",kernel_regularizer=tf.keras.regularizers.l2(l2=1e-3))(input1)
+    relu1 = tf.keras.layers.ReLU()(conv1)
+    conv2 = tf.keras.layers.Conv3D(filters=32, kernel_size=3, activation="relu", kernel_regularizer=tf.keras.regularizers.l2(l2=1e-3))(relu1)
+    bat1 = tf.keras.layers.BatchNormalization(axis=2)(conv2)
+    relu2= tf.keras.layers.ReLU()(bat1)
+    max1 = tf.keras.layers.MaxPool3D(pool_size=2,  strides=2)(relu2)
 
-    x = tensorflow.keras.layers.Conv3D(filters=64, kernel_size=3, activation="relu", kernel_regularizer=tensorflow.keras.regularizers.l2(l2=1e-3))(x)
-    x= tensorflow.keras.layers.ReLU()(x)
-    x = tensorflow.keras.layers.Conv3D(filters=64, kernel_size=3, activation="relu", kernel_regularizer=tensorflow.keras.regularizers.l2(l2=1e-3))(x)
-    x = tensorflow.keras.layers.BatchNormalization(axis=2)(x)
-    x= tensorflow.keras.layers.ReLU()(x)
-    x = tensorflow.keras.layers.MaxPool3D(pool_size=2,  strides=2)(x)
+    conv3 = tf.keras.layers.Conv3D(filters=64, kernel_size=3, activation="relu", kernel_regularizer=tf.keras.regularizers.l2(l2=1e-3))(max1)
+    relu3= tf.keras.layers.ReLU()(conv3)
+    conv4 = tf.keras.layers.Conv3D(filters=64, kernel_size=3, activation="relu", kernel_regularizer=tf.keras.regularizers.l2(l2=1e-3))(relu3)
+    bat2 = tf.keras.layers.BatchNormalization(axis=2)(conv4)
+    relu4= tf.keras.layers.ReLU()(bat2)
+    max2 = tf.keras.layers.MaxPool3D(pool_size=2,  strides=2)(relu4)
 
-    x = tensorflow.keras.layers.Conv3D(filters=128, kernel_size=3, activation="relu",kernel_regularizer=tensorflow.keras.regularizers.l2(l2=1e-3))(x)
-    x = tensorflow.keras.layers.BatchNormalization(axis=2)(x)
-    x= tensorflow.keras.layers.ReLU()(x)
-    x = tensorflow.keras.layers.MaxPool3D(pool_size=2,  strides=2)(x)
+    conv5 = tf.keras.layers.Conv3D(filters=128, kernel_size=3, activation="relu",kernel_regularizer=tf.keras.regularizers.l2(l2=1e-3))(max2)
+    bat3 = tf.keras.layers.BatchNormalization(axis=2)(conv5)
+    relu5= tf.keras.layers.ReLU()(bat3)
+    max3= tf.keras.layers.MaxPool3D(pool_size=2,  strides=2)(relu5)
 
-    x = tensorflow.keras.layers.Flatten()(x)
-    x = tensorflow.keras.layers.Dropout(0.1)(x)
-    outputs = tensorflow.keras.layers.Dense(units=1, activation="sigmoid")(x)
+    flat1 = tf.keras.layers.Flatten()(max3)
+    drop1 = tf.keras.layers.Dropout(0.1)(flat1)
+    output1 = tf.keras.layers.Dense(units=1, activation="sigmoid")(drop1)
 
     # Define the model.
-    model = tensorflow.keras.Model(inputs, outputs, name="3dcnn")
+    model = tf.keras.Model(input1, output1, name="3dcnn")
     return model
 
 
@@ -119,17 +119,18 @@ if __name__=='__main__':
     dataset_path_metadata = "AD_CTRL_metadata_labels.csv"
 
     # Import csv data
-    df, head, dic_info = import_csv(dataset_path_metadata)
+    df, head, dict_age, dict_mmse = import_csv(dataset_path_metadata)
     features = ['DXGROUP', 'ID', 'AGE', 'MMSE']
     print(df[features])
 
-    # import images, labels and file names
-    X_o, Y, fnames_AD, fnames_CTRL, file_id, file_age = read_dataset(dataset_path_AD_ROI, dataset_path_CTRL_ROI, dic_info, str_1='1', str_2='.')
-    #Normalization of intensity voxel values
+
+    # import images, labels, file names, age and mmse
+    X_o, Y, fnames_AD, fnames_CTRL, file_id, age, mmse = read_dataset(dataset_path_AD_ROI, dataset_path_CTRL_ROI,dict_age, dict_mmse , str_1='1', str_2='.')
+
     X_o=normalize(X_o)
 
     # Define ROI
-    X=X_o[:,36:86,56:106,24:74] #ippocampo
+    X=X_o[:,35:85,50:100,25:75] #ippocampo
     #X=X_o[:,11:109,12:138,24:110] #bordi neri precisi
     #X=X_o[:,20:100,20:130,20:100]
     #X=X_o
@@ -145,40 +146,47 @@ if __name__=='__main__':
     X_train_tot, Y_train_tot=stack_train_augmentation(X_train, array_img, Y_train, labels)
 
     # Augement the images of one dimension
-    X_train_tot = tensorflow.expand_dims(X_train_tot, axis=-1)
-    X_test = tensorflow.expand_dims(X_test, axis=-1)
+    X_train_tot = tf.expand_dims(X_train_tot, axis=-1)
+    X_test = tf.expand_dims(X_test, axis=-1)
 
     # Build the model
-    model = get_model(width=X.shape[1], height=X.shape[2], depth=X.shape[3])
+    model = inner_model(width=X.shape[1], height=X.shape[2], depth=X.shape[3])
     model.summary()
+
 
     # Set the learning Rate
     initial_learning_rate = 0.001
-    ReduceLROnPlateau=ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=2, verbose=1)
+    reduce_Rl=tf.keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=2, verbose=1)
+
 
     # Compile the model
     model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=initial_learning_rate), loss='binary_crossentropy', metrics=['MAE'])
 
     # Define callbacks
     checkpoint_cb = tf.keras.callbacks.ModelCheckpoint(
-            "3d_image_classification_{val_MAE}_Hipp.h5", save_best_only=True
+            "3d_regression_transfer_{val_MAE}_Hipp.h5", save_best_only=True
     )
+
     early_stopping = tf.keras.callbacks.EarlyStopping(monitor="val_loss", patience=10, verbose=1)
 
     #Fit the data
-    history=model.fit(X_train_tot,Y_train_tot, validation_split=0.1, batch_size=32, shuffle=TRUE, epochs=2, callbacks=[checkpoint_cb, early_stopping, ReduceLROnPlateau])
+    history=model.fit(X_train_tot,Y_train_tot, validation_split=0.1, batch_size=32, shuffle=True, epochs=2, callbacks=[checkpoint_cb, early_stopping, reduce_Rl])
+
+    model.save_weights('CNN_weights.h5')
 
     #history contains information about the training
     print(history.history.keys())
 
-    #Show the loss vs epoch for both validation and training samples.
-    plt.plot(history.history["val_loss"])
-    plt.plot(history.history["loss"])
-    plt.plot(history.history["MAE"])
-    plt.plot(history.history["val_MAE"])
-    plt.legend()
-    plt.yscale('log')
-    plt.show()
+    fig, ax = plt.subplots(1, 2, figsize=(20, 3))
+    ax = ax.ravel()
+
+    for i, metric in enumerate(["MAE", "loss"]):
+        ax[i].plot(model.history.history[metric])
+        ax[i].plot(model.history.history["val_" + metric])
+        ax[i].set_title("Model {}".format(metric))
+        ax[i].set_xlabel("epochs")
+        ax[i].set_ylabel(metric)
+        ax[i].legend(["train", "val"])
 
     #Display ROC curve and calculate AUC
     auc = roc_curve(X_test, Y_test, model)
